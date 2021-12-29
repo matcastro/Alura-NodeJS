cce787825826d1d5305158767e9f2cde56d6694b
Mateus Castro <matcastro2004@yahoo.com.br>
2021-12-28 22:01:59 -0300

    Finalizando CRUD e melhorando responses

--- nodejs-rest/controllers/atendimentos.js
+++ nodejs-rest/controllers/atendimentos.js
@@ -1,9 +1,27 @@
 const Atendimento = require('../models/atendimentos')
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
+    app.get('/atendimentos', (_, res) => {
+        Atendimento.lista(res)
+    })
+
+    app.get('/atendimentos/:id', (req, res) => {
+        Atendimento.buscaPorId(req.params.id, res)
+    })
+
 
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body
         Atendimento.adiciona(atendimento, res)
+    })
+
+    app.patch('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+        Atendimento.altera(id, valores, res)
+    })
+
+    app.delete('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        Atendimento.deleta(id, res)
     })
 }
--- nodejs-rest/models/atendimentos.js
+++ nodejs-rest/models/atendimentos.js
@@ -1,4 +1,5 @@
 const moment = require('moment')
+const conexao = require('../infrastructure/connections')
 const connection = require('../infrastructure/connections')
 
 class Atendimento{
@@ -36,10 +37,61 @@ class Atendimento{
                 if(error){
                     res.status(400).json(error)
                 } else{
-                    res.status(201).json(resultado)
+                    const id = resultado.insertId
+                    res.status(201).json({id, ...atendimento})
                 }
             })
         }
+    }
+
+    lista(res){
+        const sql = 'SELECT * FROM Atendimentos'
+
+        connection.query(sql, (error, resultado) =>{
+            if(error){
+                res.status(400).json(error)
+            }else{
+                res.json(resultado)
+            }
+        })
+    }
+
+    buscaPorId(id, res){
+        const sql = `SELECT * FROM Atendimentos WHERE id = ${id}`
+        
+        connection.query(sql, (error, resultado) =>{
+            if(error){
+                res.status(400).json(error)
+            }else{
+                const atendimento = resultado[0]
+                res.status(200).json(atendimento)
+            }
+        })
+    }
+
+    altera(id, valores, res){
+        if(valores.data){
+            valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
+        }
+        const sql = 'UPDATE Atendimentos SET ? WHERE id=?'
+        connection.query(sql, [valores, id], (error, resultado) =>{
+            if(error){
+                res.status(400).json(error)
+            }else{
+                res.status(200).json({id, ...valores})
+            }
+        })
+    }
+
+    deleta(id, res){
+        const sql = 'DELETE FROM Atendimentos WHERE id=?'
+        connection.query(sql, id, (error, resultado) =>{
+            if(error){
+                res.status(400).json(error)
+            }else{
+                res.status(200).json({id})
+            }
+        })
     }
 }
 
