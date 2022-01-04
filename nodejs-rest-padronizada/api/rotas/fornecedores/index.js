093d4805b3fce41f2f15e4271abc0152dbb7e1e4
Mateus Castro <matcastro2004@yahoo.com.br>
2022-01-04 08:50:48 -0300

    Seguindo com o CRUD na API para consulta, cadastro e alteração.

--- nodejs-rest-padronizada/api/rotas/fornecedores/index.js
+++ nodejs-rest-padronizada/api/rotas/fornecedores/index.js
@@ -1,9 +1,44 @@
 const roteador = require('express').Router()
 const TabelaFornecedor = require('./TabelaFornecedor')
+const Fornecedor = require('./Fornecedor')
 
-roteador.use('/', async (req, res) => {
+roteador.get('/', async (req, res) => {
     const resultados = await TabelaFornecedor.listar()
     res.json(resultados)
+})
+
+roteador.post('/', async (req, res) => {
+    const dadosRecebidos = req.body
+    const fornecedor = new Fornecedor(dadosRecebidos)
+    await fornecedor.criar()
+
+    res.json(fornecedor)
+})
+
+roteador.get('/:idFornecedor', async (req, res) => {
+    try{
+        const id = req.params.idFornecedor
+        const fornecedor = new Fornecedor({ id: id })
+        await fornecedor.carregar()
+        res.json(fornecedor)
+    } catch (erro){
+        res.json({
+            mensagem: erro.message
+        }).status(400)
+    }
+})
+
+roteador.put('/:idFornecedor', async (req, res) => {
+    try{
+        const id = req.params.idFornecedor
+        const dadosRecebidos = req.body
+        const fornecedor = new Fornecedor({ id, ...dadosRecebidos })
+        await fornecedor.atualizar()
+        res.end()    
+    } catch(erro) {
+        res.json({mensagem: erro.message}).status(400)
+    }
+    
 })
 
 module.exports = roteador
