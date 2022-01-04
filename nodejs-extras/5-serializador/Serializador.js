fc0bc383b1247333385c4207346c6d82cfe55fd0
Mateus Castro <matcastro2004@yahoo.com.br>
2022-01-04 14:49:28 -0300

    Exercício extra de serialização

---
+++ nodejs-extras/5-serializador/Serializador.js
@@ -0,0 +1,47 @@
+class Serializador {
+    json(dados) {
+        return JSON.stringify(dados)
+    }
+
+    serializar(dados) {
+        if (this.contentType === 'application/json') {
+            return this.json(
+                this.filtrar(dados)
+            )
+        }
+
+        throw new Error(this.contentType)
+    }
+
+    filtrarObjeto(dados) {
+        const novoObjeto = {}
+
+        this.camposPublicos.forEach((campo) => {
+            if (dados.hasOwnProperty(campo)) {
+                novoObjeto[campo] = dados[campo]
+            }
+        })
+
+        return novoObjeto
+    }
+
+    filtrar(dados) {
+        if (Array.isArray(dados)) {
+            dados = dados.map(item => {
+                return this.filtrarObjeto(item)
+            })
+        } else {
+            dados = this.filtrarObjeto(dados)
+        }
+
+        return dados
+    }
+}
+
+class SerializadorUsuario extends Serializador {
+    constructor(contentType){
+        super()
+        this.contentType = contentType
+        this.camposPublicos = ['nome']
+    }
+}
