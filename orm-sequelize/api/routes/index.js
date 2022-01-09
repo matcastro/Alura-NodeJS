90a71410db443e54d3347221586eb9d236cea37f
Mateus Castro <matcastro2004@yahoo.com.br>
2022-01-09 16:39:36 -0300

    Ajustando projeto replicando CRUD para tabelas de nivel e turma

--- orm-sequelize/api/routes/index.js
+++ orm-sequelize/api/routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const pessoas = require('./pessoasRoute')
+const niveis = require('./niveisRoute')
+const turmas = require('./turmasRoute')
 
 module.exports = app => {
     app.use(express.json())
 
     app.use(pessoas)
+    app.use(niveis)
+    app.use(turmas)
 }
